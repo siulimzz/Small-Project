58e1faef9c34f6a2c5fe326bdbcd5f324750020c
siulimzz <915366610@qq.com>
2017-07-14 15:55:38 +0800

    uniqlo
    使用bootstrap仿优衣库首页

---
+++ uniqlo/js/main.js
@@ -0,0 +1,17 @@
+﻿// JavaScript source code
+$(function(){
+    //获取要定位元素距离浏览器顶部的距离
+    var navH = $(".info").offset().top;
+
+    //滚动条事件
+    $(window).scroll(function(){
+        //获取滚动条的滑动距离
+        var scroH = $(this).scrollTop();
+        //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+        if(scroH>=navH){
+            $(".info").css({"position":"fixed","top":0});
+        }else if(scroH<navH){
+            $(".info").css({"position":"static"});
+        }
+    })
+})
